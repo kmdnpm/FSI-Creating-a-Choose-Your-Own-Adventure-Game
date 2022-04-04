19de289884aed61cefe55c1570e5574662dc2939
Kim <kmdnpm@gmail.com>
2022-04-04 18:25:32 -0400

    added conditional statements

--- index.js
+++ index.js
@@ -10,27 +10,27 @@ Do you follow it, or continue on your path?`)
         They are content with you staying, but you wonder if you should alert the world
         to this magical safe haven.`)
 
-        if(thirdAnswer === 'Stay'){
+        if(thirdAnswer === 'stay'){
             window.alert(`You live happily amongst the cats for the rest of your days.`)
         }
 
-        else if(thirdAnswer === 'Spread the word'){
+        else if(thirdAnswer === 'spread the word'){
             window.alert(`After leaving the cat colony, you are never able to find it again; 
             without proof, no one believes your story, which passes into legend nonetheless.`)
          }
     }
 
-    else if(secondAnswer == 'Continue'){
+    else if(secondAnswer == 'continue'){
         let thirdAnswer = window.prompt(`You come across a chamber that reaches upward to 
         a shining light above. There is a long, winding staircase, and a much quicker, 
         but rickety-looking ladder that leads up toward the light. Which do you take?`)
 
-        if (thirdAnswer ==='Ladder'){
+        if (thirdAnswer ==='ladder'){
             window.alert(`After ascending a few feet up the ladder, one of its rungs snaps,
              and you comedically fall through each of the rungs below. Sheepish, you return 
              home.`)
         }
-        else if(thirdAnswer ==='Staircase'){
+        else if(thirdAnswer ==='staircase'){
             window.alert(`After ascending the staircase, you discover a shiny blue stone, 
             which you take home and cherish forever.`)
         }
@@ -41,27 +41,27 @@ Do you follow it, or continue on your path?`)
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
 
-        if(secondAnswer === 'Past the dragon'){
+        if(secondAnswer === 'past the dragon'){
             let thirdAnswer = window.prompt(`The dragon wakes up and sits upright. 
             You only have a moment to respond, to stay or run:`)
 
-            if(thirdAnswer === 'Stay'){
+            if(thirdAnswer === 'stay'){
                 window.alert(`You and the dragon have an uplifting conversation about 
                 local politics and become lifelong friends.`)
             }
 
-            else if(thirdAnswer === 'Run'){
+            else if(thirdAnswer === 'run'){
                 window.alert(`Quickly, you run back to the cave's entrance. Sheepish, 
                 you return home.`)
             }
         }
 
-        else if(secondAnswer === 'Away from the dragon'){
+        else if(secondAnswer === 'away from the dragon'){
             let thirdAnswer = window.prompt(`After walking a while longer, you come across
              a shiny blue flower. It is so beautiful that you decide you must either draw 
              it or pick it. Which do you do?`)
 
-             if(thirdAnswer === 'Draw it'){
+             if(thirdAnswer === 'draw it'){
                  window.alert(`You draw the flower, capturing only a fraction of its beauty
                   with your quill. You bring the drawing home, somewhat disappointed, but 
                   over time, discover joy in sharing it with your friends and family, 
