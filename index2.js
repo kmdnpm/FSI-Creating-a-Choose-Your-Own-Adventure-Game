9ce45f5a17b926b47a704caefa62f23118f670ec
Kim <kmdnpm@gmail.com>
2022-04-04 21:10:57 -0400

    Switch-case alternative for Adventure Game

--- index2.js
+++ index2.js
@@ -1,9 +1,21 @@
 
 firstChoice();
 
+let answer = window.prompt("Would you like to play again?: [yes] or [no]")
+
+while(answer === "yes"){
+    firstChoice();
+    let answer2 = window.prompt("Play again?: [yes] or [no]")
+
+    if (answer2 === "no"){
+        break;
+    }
+}
+    window.alert("Thanks for playing! Goodbye!")
+
 
 function firstChoice(){
-    let choice  = window.prompt('Do you head left or right?')
+    let choice  = window.prompt('Do you head [left] or [right]?')
 
     switch(choice){
         case "left":
@@ -20,7 +32,7 @@ function left(){
 
     let choice = window.prompt(`You come across a stray cat. 
             It scampers off down a small hole, just large enough for you to crawl through. 
-            Do you follow it, or continue on your path?`);
+            Do you [follow] it, or [continue] on your path?`);
 ;
 
     switch(choice){
@@ -41,7 +53,7 @@ function follow(){
 
     let choice = window.prompt(`You follow the cat to a colony of cats,
         nestled in a fort of warm blankets and subsisting off inexplicably warm soup.
-        They are content with you staying, but you wonder if you should alert the world
+        They are content with you [stay]ing, but you wonder if you should [alert] the world
         to this magical safe haven.`)
 
     switch(choice){
@@ -49,7 +61,7 @@ function follow(){
             window.alert(`You live happily amongst the cats for the rest of your days.`)
             break;
 
-        case "spread the word":
+        case "alert":
             window.alert(`After leaving the cat colony, you are never able to find it again; 
             without proof, no one believes your story, which passes into legend nonetheless.`)
             break;
@@ -59,8 +71,8 @@ function follow(){
 function progress(){
 
     let choice = window.prompt(`You come across a chamber that reaches upward to 
-    a shining light above. There is a long, winding staircase, and a much quicker, 
-    but rickety-looking ladder that leads up toward the light. Which do you take?`)
+    a shining light above. There is a long, winding [staircase], and a much quicker, 
+    but rickety-looking [ladder] that leads up toward the light. Which do you take?`)
 
 
     switch(choice){
@@ -81,8 +93,8 @@ function progress(){
 function right(){
 
     let choice = window.prompt(`You come across a snoring dragon. 
-            On the other side of him, you see a shiny chest of treasure. Another path would 
-            lead you away from the dragon altogether. Which path do you take?`);
+            On the other side [past the dragon], you see a shiny chest of treasure. Another path would 
+            lead you [away from the dragon] altogether. Which path do you take?`);
 
 
     switch(choice){
@@ -99,7 +111,7 @@ function right(){
 
 function pastDragon(){
     let choice = window.prompt(`The dragon wakes up and sits upright. 
-    You only have a moment to respond, to stay or run:`)
+    You only have a moment to respond, to [stay] or [run]:`)
 
     switch(choice){
         case "stay":
@@ -116,8 +128,8 @@ function pastDragon(){
 
 function awayFromDragon(){
     let choice = window.prompt(`After walking a while longer, you come across
-    a shiny blue flower. It is so beautiful that you decide you must either draw 
-    it or pick it. Which do you do?`)
+    a shiny blue flower. It is so beautiful that you decide you must either [draw 
+    it] or [pick it]. Which do you do?`)
 
     switch(choice){
         case "draw it":
